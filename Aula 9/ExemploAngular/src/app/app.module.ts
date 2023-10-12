3003839a677e97904621a920b7df651f64b5e5af
Rafael Moreno <rafaelmorenosax@gmail.com>
2023-10-11 21:47:41 -0300

    Aula 10

--- Aula 9/ExemploAngular/src/app/app.module.ts
+++ Aula 9/ExemploAngular/src/app/app.module.ts
@@ -6,19 +6,18 @@ import { AppComponent } from './app.component';
 import { MenuSuperiorComponent } from './menu-superior/menu-superior.component';
 import { ListaClienteComponent } from './lista-cliente/lista-cliente.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuSuperiorComponent,
-    ListaClienteComponent
+    ListaClienteComponent,
+    CalculadoraComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgbModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, NgbModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
