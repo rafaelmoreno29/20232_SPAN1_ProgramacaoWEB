83bbb3ff02faedf4a1fde744a05e39a58a303814
Rafael Moreno <rafaelmorenosax@gmail.com>
2023-10-18 22:04:17 -0300

    Aula 11

--- Aula 9/ExemploAngular/src/app/app-routing.module.ts
+++ Aula 9/ExemploAngular/src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
+import { HomeComponent } from './home/home.component';
+import { ListaClienteComponent } from './lista-cliente/lista-cliente.component';
+import { TelaPaiComponent } from './tela-pai/tela-pai.component';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
+import { TelaFilho1Component } from './tela-pai/tela-filho1/tela-filho1.component';
+import { TelaFilho2Component } from './tela-pai/tela-filho2/tela-filho2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'calculadora', component: CalculadoraComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'lista-cliente', component: ListaClienteComponent },
+  {
+    path: 'tela-pai',
+    component: TelaPaiComponent,
+    children: [
+      { path: 'tela-filho1', component: TelaFilho1Component },
+      { path: 'tela-filho2', component: TelaFilho2Component },
+    ],
+  },
+  { path: '**', component: PaginaNaoEncontradaComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- Aula 9/ExemploAngular/src/app/app.module.ts
+++ Aula 9/ExemploAngular/src/app/app.module.ts
@@ -8,6 +8,11 @@ import { ListaClienteComponent } from './lista-cliente/lista-cliente.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CalculadoraComponent } from './calculadora/calculadora.component';
 import { FormsModule } from '@angular/forms';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
+import { TelaPaiComponent } from './tela-pai/tela-pai.component';
+import { HomeComponent } from './home/home.component';
+import { TelaFilho1Component } from './tela-pai/tela-filho1/tela-filho1.component';
+import { TelaFilho2Component } from './tela-pai/tela-filho2/tela-filho2.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +20,11 @@ import { FormsModule } from '@angular/forms';
     MenuSuperiorComponent,
     ListaClienteComponent,
     CalculadoraComponent,
+    PaginaNaoEncontradaComponent,
+    TelaPaiComponent,
+    HomeComponent,
+    TelaFilho1Component,
+    TelaFilho2Component,
   ],
   imports: [BrowserModule, AppRoutingModule, NgbModule, FormsModule],
   providers: [],
